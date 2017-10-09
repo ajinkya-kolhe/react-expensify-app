a0174962b1d6566d835c8a0bc0731fbb514f5bee
Ajinkya Kolhe <ajinkya.k15@gmail.com>
2017-10-09 15:32:27 +1100

    Add google Auth and private routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Header = () => (
+import { startLogout } from '../actions/auth';
+
+export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
     <NavLink exact={true} to="/" activeClassName="is-active">Dashboard</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <button onClick={startLogout}>Logout</button>
   </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+  startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
