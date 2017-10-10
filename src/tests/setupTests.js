cd27680690b082aa2f2796807006b57d9079ad8d
Ajinkya Kolhe <ajinkya.k15@gmail.com>
2017-10-10 22:58:33 +1100

    Upgrade to react 16

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,9 +1,9 @@
-// import Enzyme from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import DotEnv from 'dotenv';
 
-// Enzyme.configure({
-//   adapter: new Adapter()
-// });
+Enzyme.configure({
+  adapter: new Adapter()
+});
 
 DotEnv.config({ path: '.env.test' });
