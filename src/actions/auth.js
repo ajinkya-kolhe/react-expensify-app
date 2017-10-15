da3f4175bf9dd9e27f5f724e499c84899cebdd35
Ajinkya Kolhe <ajinkya.k15@gmail.com>
2017-10-15 23:58:01 +1100

    Added facebook authentication

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,14 +1,48 @@
-import { firebase, googleAuthProvider } from '../firebase/firebase';
+import { firebase, googleAuthProvider, facebookAuthProvider } from '../firebase/firebase';
 
 export const login = (uid) => ({
   type: 'LOGIN',
   uid
 });
 
-export const startLogin = () => {
+export const startGoogleLogin = () => {
   return () => {
     return firebase.auth().signInWithPopup(googleAuthProvider);
   };
+}
+
+export const startFacebookLogin = () => {
+  return () => {
+    return firebase.auth().signInWithPopup(facebookAuthProvider)
+        .catch((error) => handleError(error));
+  };
+}
+
+const getProvider = (provider) => {
+  switch (provider) {
+    case 'facebook.com':
+      return facebookAuthProvider;
+    default:
+      return googleAuthProvider;
+  }
+}
+
+const handleError = (error) => {
+  const pendingCredential = error.credential;
+  if (error.code === 'auth/account-exists-with-different-credential') {
+    firebase.auth().fetchProvidersForEmail(error.email)
+      .then((providers) => {
+        const provider = getProvider(providers[0]);
+        provider.setCustomParameters( {'login_hint': error.email} );
+        firebase.auth().signInWithPopup(provider)
+          .then((result) => {
+            return result.user;
+          })
+          .then((user) => {
+            return user.linkWithCredential(pendingCredential);
+          })
+      });
+  }
 }
 
 export const logout = () => ({
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { startLogin } from '../actions/auth';
+import { startGoogleLogin, startFacebookLogin } from '../actions/auth';
 
-export const LoginPage = ({ startLogin }) => (
+export const LoginPage = ({ startGoogleLogin, startFacebookLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
       <h1 className="box-layout__title">Expensify</h1>
       <p>It's time to get your expenses under control.</p>
-      <button className="button" onClick={startLogin}>Login with Google</button>
+      <button className="button" onClick={startGoogleLogin}>Login with Google</button>
+      <button className="button" onClick={startFacebookLogin}>Login with Facebook</button>
     </div>
   </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
-  startLogin: () => dispatch(startLogin())
+  startGoogleLogin: () => dispatch(startGoogleLogin()),
+  startFacebookLogin: () => dispatch(startFacebookLogin()),
 })
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
